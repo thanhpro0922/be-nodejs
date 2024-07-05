2937730700739b8e4a890e02de5d3e36f17ab2d1
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-05 13:06:44 +0700

    1.3 Chèn nhiều ảnh (Phần 2)

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,8 +18,12 @@ if (formSendData) {
         console.log(images);
         if (content || images.length > 0) {
             //@ Send content or images up server
-            socket.emit("CLIENT_SEND_MESSAGE", content);
+            socket.emit("CLIENT_SEND_MESSAGE", {
+                content: content,
+                images: images,
+            });
             e.target.elements.content.value = "";
+            upload.resetPreviewPanel();
             setTimeout(() => {
                 socket.emit("CLIENT_SEND_TYPING", "hidden");
             }, 100); //@ Bị xung đột code nên phải để 100ms
@@ -37,16 +41,29 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
     const div = document.createElement("div");
 
     let htmlFullName = "";
+    let htmlContent = "";
+    let htmlImages = "";
     if (myId == data.userId) {
         div.classList.add("inner-outgoing");
     } else {
         div.classList.add("inner-incoming");
         htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
     }
+    if (data.content) {
+        htmlContent = `<div class="inner-content">${data.content}</div>`;
+    }
+    if (data.images) {
+        htmlImages += `<div class="inner-images">`;
+        for (const image of data.images) {
+            htmlImages += `<img src="${image}"`;
+        }
+        htmlImages += `</div>`;
+    }
 
     div.innerHTML = `
-      ${htmlFullName}
-      <div class="inner-content">${data.content}</div>
+        ${htmlFullName}
+        ${htmlContent}
+        ${htmlImages}
     `;
 
     body.insertBefore(div, boxTyping);
