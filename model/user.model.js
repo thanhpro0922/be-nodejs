976aedfb0e76c90fb7a56750253cdfdc7ee72a45
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-05 17:25:16 +0700

    1.2 Phần tích chức năng (Phần 1)

--- model/user.model.js
+++ model/user.model.js
@@ -11,6 +11,12 @@ const userSchema = new mongoose.Schema(
         },
         phone: String,
         avatar: String,
+        friendList: {
+            user_id: String,
+            room_chat_id: String,
+        },
+        acceptFriends: Array,
+        requestFriends: Array,
         status: {
             type: String,
             default: "active",
