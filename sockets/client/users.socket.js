4248f34acd6f67b71112b5a4490c7f1ed827ad0e
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-09 09:27:07 +0700

    1.3 Hiển thị A vào danh sách Lời mời kết bạn của B

--- sockets/client/users.socket.js
+++ sockets/client/users.socket.js
@@ -52,6 +52,16 @@ module.exports = async (res) => {
                 userId: userId,
                 lengthAcceptFriends: lengthAcceptFriends,
             });
+
+            //** Get info of A return for B
+            const infoUserA = await User.findOne({
+                _id: myUserId,
+            }).select("id avatar fullName");
+
+            socket.broadcast.emit("SERVER_RETURN_INFO_ACCEPT_FRIEND", {
+                userId,
+                infoUserA,
+            });
         });
 
         //* Người dùng hủy gửi yêu cầu kết bạn
