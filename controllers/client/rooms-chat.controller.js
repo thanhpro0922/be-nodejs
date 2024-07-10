7bbc010621ba4da8600198453af6b84b842bcf69
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-10 13:49:38 +0700

    2.2 Tạo phòng chat

--- controllers/client/rooms-chat.controller.js
+++ controllers/client/rooms-chat.controller.js
@@ -1,7 +1,53 @@
+const User = require("../../model/user.model");
+const RoomChat = require("../../model/room-chat.model");
+
 //@ [GET] /rooms-chat/
 module.exports.index = async (req, res) => {
     res.render("client/pages/rooms-chat/index", {
-      pageTitle: "Danh sách phòng",
+        pageTitle: "Danh sách phòng",
     });
-  };
-  
+};
+
+//@ [GET] /rooms-chat/create
+module.exports.create = async (req, res) => {
+    const friendList = res.locals.user.friendList;
+
+    for (const friend of friendList) {
+        const infoFriend = await User.findOne({
+            _id: friend.user_id,
+        }).select("fullName avatar");
+        friend.infoFriend = infoFriend;
+    }
+
+    res.render("client/pages/rooms-chat/create", {
+        pageTitle: "Tạo phòng",
+        friendList: friendList,
+    });
+};
+
+//@ [POST] /room-chat/create
+module.exports.createPost = async (req, res) => {
+    const title = req.body.title;
+    const usersId = req.body.usersId;
+
+    let dataChat = {
+        title: title,
+        typeRoom: "group",
+        users: [],
+    };
+
+    usersId.forEach((userId) => {
+        dataChat.users.push({
+            user_id: userId,
+            role: "user",
+        });
+    });
+    dataChat.users.push({
+        user_id: res.locals.user.id,
+        role: "superAdmin",
+    });
+    const room = new RoomChat(dataChat);
+    await room.save();
+
+    res.redirect(`/chat/${room.id }`);
+};
--- routes/client/rooms-chat.route.js
+++ routes/client/rooms-chat.route.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const controller = require("../../controllers/client/rooms-chat.controller");
 
 router.get("/", controller.index);
+router.get("/create", controller.create);
+router.post("/create", controller.createPost);
 
 module.exports = router;
