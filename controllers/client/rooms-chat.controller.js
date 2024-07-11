9c03701540c5a35886288cc82c636e7f13da8f5e
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-11 10:07:00 +0700

    2.3 Hiển thị danh sách phòng chat

--- controllers/client/rooms-chat.controller.js
+++ controllers/client/rooms-chat.controller.js
@@ -3,8 +3,16 @@ const RoomChat = require("../../model/room-chat.model");
 
 //@ [GET] /rooms-chat/
 module.exports.index = async (req, res) => {
+    const userId = res.locals.user.id;
+
+    const listRoomChat = await RoomChat.find({
+        "users.user_id": userId,
+        typeRoom: "group",
+        deleted: false,
+    });
     res.render("client/pages/rooms-chat/index", {
         pageTitle: "Danh sách phòng",
+        listRoomChat: listRoomChat,
     });
 };
 
@@ -49,5 +57,5 @@ module.exports.createPost = async (req, res) => {
     const room = new RoomChat(dataChat);
     await room.save();
 
-    res.redirect(`/chat/${room.id }`);
+    res.redirect(`/chat/${room.id}`);
 };
